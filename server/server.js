eb34a5807e2cc09ef9915b92e96a39d2d17763bc
vicse <vicse.ore@tecsup.edu.pe>
2019-04-22 19:12:33 -0500

    Restserver - Usuario completado

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
+
 const app = express();
 
 const bodyParser = require('body-parser')
@@ -11,43 +14,17 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(require('./routes/usuario'));
 
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario')
-});
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true },
+    (err, res) => {
 
-app.post('/usuario', function(req, res) {
+        if (err) throw err;
 
-    let body = req.body;
+        console.log('Base de datos online');
 
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        })
-
-    } else {
-
-        res.json({
-            persona: body
-        });
-    }
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
     });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario')
-});
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto :', process.env.PORT);
